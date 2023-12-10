c6f364a6a00815e7268261c8634400f37328d5d6
greffgreff <greffchandler80@gmail.com>
2023-12-10 01:52:35 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect, useState } from "react";
 
-function App() {
+const baseUrl = "https://testing.tinyfilesharing.com";
+
+export const App = () => {
+  const { loginWithRedirect, logout, getAccessTokenSilently, isAuthenticated } = useAuth0();
+  const [token, setToken] = useState<string>();
+  const [records, setRecords] = useState();
+  const [details, setDetails] = useState();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      getAccessTokenSilently()
+        .then(token => {
+          console.log(token);
+          setToken(token);
+        })
+        .catch(console.log);
+    }
+  }, [getAccessTokenSilently, isAuthenticated]);
+
+  const handleLogout = () => {
+    logout({ logoutParams: { returnTo: window.location.origin } });
+  };
+
+  const handleLogin = () => {
+    loginWithRedirect();
+  };
+
+  const fetchRecords = async () => {
+    try {
+      const res = await axios.get(baseUrl + "/api/storage/files/list?offset=0&count=100", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      setRecords(res.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const fetchDetails = async () => {
+    try {
+      const res = await axios.get(baseUrl + "/api/storage/details", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      setDetails(res.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!isAuthenticated ? (
+        <input type="button" value="Login" onClick={handleLogin} />
+      ) : (
+        <>
+          <input type="button" value="Logout" onClick={handleLogout} />
+          <input type="button" value="fetch detail" onClick={fetchDetails} />
+          <input type="button" value="fetch records" onClick={fetchRecords} />
+          {details && <pre>{JSON.stringify(details, null, 2)}</pre>}
+          {records && <pre>{JSON.stringify(records, null, 2)}</pre>}
+        </>
+      )}
     </div>
   );
-}
-
-export default App;
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
+import { createRoot } from "react-dom/client";
+import { App } from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-unodm5dngxg4hqwm.eu.auth0.com"
+    clientId="UmUEFly5Zu4OvKr5axHsTYVG6hpBXPU6"
+    authorizationParams={{
+      audience: "https://tinyfilesharing.com/",
+      redirect_uri: window.location.origin,
+      scope: "openid profile email",
+    }}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
