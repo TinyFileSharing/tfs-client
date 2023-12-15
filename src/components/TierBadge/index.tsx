142a500e2b7f7c5842b8982c10ef1e8e77c2ede1
greffgreff <greffchandler80@gmail.com>
2023-12-15 02:04:02 +0100

    Added `upgrade tier` label

--- src/components/TierBadge/index.tsx
+++ src/components/TierBadge/index.tsx
@@ -1,4 +1,5 @@
 import './styles.css'
+import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome'
 
 interface TierBadgeProps {
    label: string
@@ -7,5 +8,14 @@ interface TierBadgeProps {
 export const TierBadge = ({ label }: TierBadgeProps) => {
    const isFreemium = label === 'tier:0'
    const name = label === 'tier:0' ? 'freemium' : label?.includes('tier:') ? 'premium' : label
-   return <div className={'tier-badge' + (isFreemium ? ' freemium' : ' pro')}>{name}</div>
+   return (
+      <div className='tier-badge'>
+         <div className={'tier-label' + (isFreemium ? ' freemium' : ' pro')}>{name}</div>
+         {isFreemium && (
+            <span className='upgrade-tier-prompt'>
+               <AutoAwesomeIcon style={{ fontSize: 20, color: '#e3007a' }} /> Upgrade tier
+            </span>
+         )}
+      </div>
+   )
 }
